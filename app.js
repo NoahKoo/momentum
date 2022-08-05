6f19148e0168634a218eff236989b603ab0796e4
NoahKoo <103397887+NoahKoo@users.noreply.github.com>
2022-08-05 13:30:27 +0900

    practice object calculating

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const player = {
-  name: "noah",
-  sayHello: function (otherPersonsName) {
-    console.log("hello " + otherPersonsName + " nice to meet you!");
-  },
-};
+const calculator = {
+    add: function(a, b){
+    console.log(a+b);
+    },
+    minus: function(a, b){
+      console.log(a-b);
+    },
+    divide: function(a, b){
+      console.log(a/b);
+    square: function(a, b){
+      console.log(a**b);    }
+    }
+  }
 
-player.sayHello("tim");
-player.sayHello("tom");
+  calculator.add(a, b)
+  calculator.minus(a, b)
+  calculator.divide(a, b)
+  calculator.square(a, b)
