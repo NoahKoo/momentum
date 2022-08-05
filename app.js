494e2de0dc8ac33dfbab53a486914e3930ce7f68
NoahKoo <103397887+NoahKoo@users.noreply.github.com>
2022-08-05 13:43:59 +0900

    correcting error

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const calculator = {
-    add: function(a, b){
-    console.log(a+b);
-    },
-    minus: function(a, b){
-      console.log(a-b);
-    },
-    divide: function(a, b){
-      console.log(a/b);
-    square: function(a, b){
-      console.log(a**b);    }
-    }
-  }
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  square: function (a, b) {
+    console.log(a ** b);
+  },
+};
 
-  calculator.add(a, b)
-  calculator.minus(a, b)
-  calculator.divide(a, b)
-  calculator.square(a, b)
+calculator.add(1, 1);
+calculator.minus(5, 5);
+calculator.divide(5, 5);
+calculator.square(5, 5);
