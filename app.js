c8084625626fdc704e57af25ebe2614870663555
NoahKoo <103397887+NoahKoo@users.noreply.github.com>
2022-08-05 16:21:44 +0900

    conditional 1

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-const calculator = {
-  add: function (a, b) {
-    console.log(a + b);
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  divide: function (a, b) {
-    console.log(a / b);
-  },
-  square: function (a, b) {
-    console.log(a ** b);
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-calculator.add(1, 1);
-calculator.minus(5, 5);
-calculator.divide(5, 5);
-calculator.square(5, 5);
+if (isNaN(age)) {
+  console.log("Please wirte a number");
+} else {
+  console.log("Thank you for writing your age.");
+}
