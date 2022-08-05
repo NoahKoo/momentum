cf2b918a6bb4e4ca394799d410b76201d384adb2
NoahKoo <103397887+NoahKoo@users.noreply.github.com>
2022-08-05 16:47:17 +0900

    conditional 2

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const age = parseInt(prompt("How old are you?"));
 
-if (isNaN(age)) {
-  console.log("Please wirte a number");
-} else {
-  console.log("Thank you for writing your age.");
+if (isNaN(age) || age < 0) {
+  console.log("Please wirte a positive number");
+} else if (age < 18) {
+  console.log("You are too young to smoke.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can smoke");
+} else if (age > 50 && age <= 80) {
+  console.log("You'd better not to smoke");
+} else if (age > 80) {
+  console.log("Just do whatever you want");
 }
