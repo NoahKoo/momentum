73659f2983a24198534acc450d1ddf0853be1992
NoahKoo <103397887+NoahKoo@users.noreply.github.com>
2022-08-09 22:14:03 +0900

    title click event

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector("div.hello:first-child h1");
 
-if (isNaN(age) || age < 0) {
-  console.log("Please wirte a positive number");
-} else if (age < 18) {
-  console.log("You are too young to smoke.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can smoke");
-} else if (age > 50 && age <= 80) {
-  console.log("You'd better not to smoke");
-} else if (age === 100) {
-  console.log("Wow you are wise");
-} else if (age > 80) {
-  console.log("Just do whatever you want");
+function handleTitleClick() {
+  console.log("title was clicked!");
 }
+title.addEventListener("click", handleTitleClick);
