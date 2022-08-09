688fe54894c514da71923b546fcbc36d2bc59ec4
NoahKoo <103397887+NoahKoo@users.noreply.github.com>
2022-08-09 19:05:14 +0900

    adding else if

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ if (isNaN(age) || age < 0) {
   console.log("You can smoke");
 } else if (age > 50 && age <= 80) {
   console.log("You'd better not to smoke");
+} else if (age === 100) {
+  console.log("Wow you are wise");
 } else if (age > 80) {
   console.log("Just do whatever you want");
 }
