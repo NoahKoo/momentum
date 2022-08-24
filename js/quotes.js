b861da8a3f43e9af72c7651576aabe74066d0362
NoahKoo <103397887+NoahKoo@users.noreply.github.com>
2022-08-24 18:13:08 +0900

    Making Quotes

---
+++ js/quotes.js
@@ -0,0 +1,57 @@
+const quotes = [
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote:
+      "The world is a book and those who do not travel read only one page.",
+    author: "Saint Augustine",
+  },
+  {
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote: "They tried to bury us, they didn't know we were seeds.",
+    author: "Mexican Proveb",
+  },
+  {
+    quote:
+      "If you set your goals ridiculously high and it's a failuure, you will fail above everyone else's success.",
+    author: "James Cameron",
+  },
+  {
+    quote:
+      "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+    author: "Oprah Winfrey",
+  },
+  {
+    quote:
+      "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+    author: "Mother Teresa",
+  },
+  {
+    quote:
+      "It is during our darkest moments that we must focus to see the light.",
+    author: "Aristotle",
+  },
+  {
+    quote:
+      "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+    author: "Ralph Waldo Emerson",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
