64d89ac6e8c7dc88e602feaaa5857e4826823350
NoahKoo <103397887+NoahKoo@users.noreply.github.com>
2022-09-05 04:56:13 +0900

    Location&&Weather

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "a3a59914321e935a4e556772e51978d5";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
