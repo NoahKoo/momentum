0b96de87faed46332aa47b2707a51a3d799ca7d2
NoahKoo <103397887+NoahKoo@users.noreply.github.com>
2022-09-10 20:10:21 +0900

    making correcting

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const logoutBtn = document.querySelector(".btn-logout");
+
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
@@ -8,17 +10,24 @@ function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("USERNAME_KEY", username);
+  localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
   window.location.href = "list.html";
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `${username}님, 잠시만 기다려주세요.`;
+  greeting.innerText = `어서오세요 ${username}님, 환영합니다.`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+function onLogoutSubmit(event) {
+  savedUsername();
+  localStorage.removeItem(USERNAME_KEY);
+  window.location.href = "index.html";
+  window.location.clear();
+}
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
@@ -26,3 +35,5 @@ if (savedUsername === null) {
 } else {
   paintGreetings(savedUsername);
 }
+
+logoutBtn.addEventListener("submit", onLogoutSubmit);
